932106fe94769aed525f49e3ba415ffcd66f7736
swami2929 <wobin33891@vkr1.com>
2024-01-17 16:43:24 +0530

    first commit

---
+++ css/javascript/index.js
@@ -0,0 +1,8 @@
+// let num1=prompt("enter a value")
+// console.log(typeof(num1))
+// let num2 = prompt("enter a value")
+// console.log(num1+num2)
+// console.log("my first javascript")
+// var a=30
+// var  a=40
+// console.log(a)
